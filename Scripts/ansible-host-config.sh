79cf0909b2e379473aa8f1aeae1d2c1cab78301f
Yakov Donde <86574080+yakovdonde@users.noreply.github.com>
2024-06-24 23:47:12 +0400

    22-june-24 EOD

--- Scripts/ansible-host-config.sh
+++ Scripts/ansible-host-config.sh
@@ -2,5 +2,4 @@ cd ~/DevOps-Portfolio/kubespray && \
 sudo apt update && \
 sudo apt install git python3 python3-pip -y && \
 git clone https://github.com/kubernetes-incubator/kubespray.git && \
-cd kubespray &&& \
 pip install -r requirements.txt
